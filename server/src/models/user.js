9ac64861a5243888eca8ae2430dbf7ed6c518648
sunilkandpal1o1 <sskandpal.uk@gmail.com>
2020-08-20 19:04:00 +0530

    User model

---
+++ server/src/models/user.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema, mongo } from 'mongoose';
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+
+// export let User = mongoose.model('user', userSchema);
+
+module.exports = mongoose.model('User', userSchema);
