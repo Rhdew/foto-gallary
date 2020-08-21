b388b2dd57d6f6c92e04e3c2145db646a642e8c9
rhdew <rdewari07@gmail.com>
2020-08-21 17:48:41 +0530

    resolver of createUser

---
+++ server/src/graphql/resolvers/user/mutation.js
@@ -0,0 +1,34 @@
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import User from '../../../models/user';
+import {UserInputError} from 'apollo-server-express';
+
+const createUser = async (parent, args)=>{
+    if(!validator.isAlphanumeric(args.user.username)){
+        throw UserInputError('username should be alphanumeric');
+    }
+    if(validator.isEmpty(args.user.username , { ignore_whitespace:true})){
+        throw UserInputError('username can not be empty');
+    }
+    if(validator.isEmpty(args.user.password, { ignore_whitespace:true})){
+        throw UserInputError('username can not be empty');
+    }
+    if(validator.isEmpty(args.user.email, { ignore_whitespace:true})){
+        throw UserInputError('email can not be empty');
+    }
+    if(!validator.isEmail(args.user.email)){
+        throw UserInputError('email should be valid');
+    }
+    
+    const user = await User.findOne({ username: args.user.username });
+    if (user) {
+        throw new UserInputError('Username already exist');
+    }
+
+    const hashedPassword = await bcrypt.hash(args.user.password, 10);
+    return User.create({ username: args.user.username, password: hashedPassword, email: args.user.email });
+};
+
+module.exports = {
+    createUser,
+}
