063cc52180f31296ec727810cf18a9801b77a6e7
rhdew <rdewari07@gmail.com>
2020-08-21 17:58:55 +0530

    create user schema

--- server/src/graphql/resolvers/index.js
+++ server/src/graphql/resolvers/index.js
@@ -1,7 +1,15 @@
+import user from './user'
+
 const resolvers = {
     Query: {
         hello: () => `Hello world!`,
+    },
+
+    Mutation: {
+        hi: () => 'hey',
+        createUser: user.createUser,
     }
+    
 }
 
 export { resolvers as default };
