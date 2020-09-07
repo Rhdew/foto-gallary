6c3392fc661ed7a7acdc4e42a056e904598c1369
sunilkandpal1o1 <sskandpal.uk@gmail.com>
2020-09-07 13:54:43 +0530

    Fixed createPost mutation to handle image uploads

--- server/src/graphql/resolvers/post/mutation.js
+++ server/src/graphql/resolvers/post/mutation.js
@@ -1,6 +1,5 @@
 import { AuthenticationError } from 'apollo-server-express';
 import cloudinary from 'cloudinary';
-import path from 'path';
 import User from '../../../models/user';
 import Post from '../../../models/post';
 
@@ -8,23 +7,36 @@ const createPost = async (parent, { post }, { user }) => {
   if (!user) {
     throw new AuthenticationError('user is not logged in');
   }
+
   const currentUser = await User.findOne({ username: user.username });
 
-  const { createReadStream } = await post.image;
+  const { createReadStream } = await post.image.file;
   const stream = createReadStream();
 
-  const mainDir = path.dirname(require.main.filename);
-  const fileName = `${mainDir}/images/${post.image}`;
-  // const fileName = `__dirname/../../../../images/${args.post.image}`;
+  let createdPost = '';
 
-  const photo = await stream.pipe(cloudinary.v2.uploader.upload_stream());
+  try {
+    const res = await new Promise((resolve, reject) => {
+      stream.pipe(
+        cloudinary.v2.uploader.upload_stream((error, result) => {
+          if (error) {
+            reject(error);
+          }
 
-  const createdPost = await Post.create({
-    caption: post.caption,
-    image: photo.secure_url,
-    imagePublicId: photo.public_id,
-    author: currentUser.id,
-  });
+          resolve(result);
+        }),
+      );
+    });
+
+    createdPost = await Post.create({
+      caption: post.caption,
+      image: res.secure_url,
+      imagePublicId: res.public_id,
+      author: currentUser.id,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 
   return createdPost;
 };
